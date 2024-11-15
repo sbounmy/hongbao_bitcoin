b34f111b6e7ca2fe171687caf79df2f363cb1d78
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2024-11-15 16:12:03 +0100

    added seo friendly

---
+++ app/controllers/og_image_controller.rb
@@ -0,0 +1,16 @@
+class OgImageController < ApplicationController
+  # Skip any authentication if you have it
+
+  # Skip CSRF protection as this is just for viewing
+  skip_before_action :verify_authenticity_token
+
+  layout false # Don't use the application layout
+
+  def show
+    # Set response headers for better caching
+    response.headers["Cache-Control"] = "public, max-age=31536000"
+    response.headers["Content-Type"] = "text/html; charset=utf-8"
+
+    render "og_image/show"
+  end
+end
