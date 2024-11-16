f27ad9bcf85f6422150d102ce4c5e7493fb889b8
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2024-11-16 07:47:25 +0100

    added donate section

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["address"]
+  static values = {
+    successMessage: String
+  }
+
+  copy() {
+    navigator.clipboard.writeText(this.addressTarget.textContent.trim())
+      .then(() => {
+        alert(this.successMessageValue)
+      })
+      .catch((err) => {
+        console.error('Failed to copy:', err)
+      })
+  }
+} 
