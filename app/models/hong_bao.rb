48a8b499200e9f6eb63b0d2da00e935f258a9c08
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2024-11-19 12:32:34 +0100

    generate the signature fot mt_pelerin with openssl 3.0

--- app/models/hong_bao.rb
+++ app/models/hong_bao.rb
@@ -1,18 +1,31 @@
 class HongBao < ApplicationRecord
   belongs_to :paper
 
-  # encrypts :private_key
-
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :paper_id, presence: true
 
   before_create :generate_bitcoin_keys, :generate_mt_pelerin_request
 
+  # Store JSON fields for Mt Pelerin API responses and requests
   store :mt_pelerin_response, accessors: [ :id, :amount, :currency, :address, :hash, :external_id ], prefix: true
   store :mt_pelerin_request, accessors: [ :hash, :code, :message ], prefix: true
 
+  # Elliptic curve used by Bitcoin
+  CURVE = "secp256k1"
+
+  # Converts a public key to a Bitcoin address
+  # @return [String] Bitcoin address in base58check format
+  def bitcoin_address
+    # Convert public key to hash160 (RIPEMD160(SHA256(public_key)))
+    hash160 = Bitcoin.hash160([ public_key ].pack("H*"))
+    # Encode with version byte to get final Bitcoin address
+    Bitcoin.encode_address(hash160, Bitcoin.network[:pubkey_version])
+  end
+
   private
 
+  # Generates a new Bitcoin keypair and stores it
+  # Called before create as part of before_create callback
   def generate_bitcoin_keys
     key = Bitcoin::Key.generate
     self.public_key = key.pub
@@ -20,18 +33,64 @@ class HongBao < ApplicationRecord
     self.address = key.addr
   end
 
+  # Generates Mt Pelerin request signature
+  # Creates a random 4-digit code and signs "MtPelerin-{code}" message
+  # Called before create as part of before_create callback
   def generate_mt_pelerin_request
-    begin
-      self.mt_pelerin_request_code ||= rand(1000..9999).to_s
-      message = "MtPelerin-#{mt_pelerin_request_code}"
-      key = Bitcoin::Key.new(private_key)
-      puts "key: #{key.priv.inspect}"
-      signature = key.sign_message(message)
-      self.mt_pelerin_request_hash = Base64.strict_encode64(signature)
-    rescue => e
-      Rails.logger.error "Mt Pelerin Hash Generation Error: #{e.class}"
-      Rails.logger.error e.message
-      Rails.logger.error e.backtrace.join("\n")
-    end
+    self.mt_pelerin_request_code ||= rand(1000..9999).to_s
+    message = "MtPelerin-#{mt_pelerin_request_code}"
+
+    # Sign the message using OpenSSL EC key
+    key = pkey_from_private_key(private_key)
+    signature = key.dsa_sign_asn1(message)
+    self.mt_pelerin_request_hash = Base64.strict_encode64(signature)
+  end
+
+  # Creates an OpenSSL EC key from a private key
+  # @param priv [String] Private key in hex format
+  # @return [OpenSSL::PKey::EC] EC key ready for signing
+  def pkey_from_private_key(priv)
+    # Get corresponding public key
+    pub = restore_public_key priv
+
+    group = OpenSSL::PKey::EC::Group.new(CURVE)
+
+    # Convert keys to OpenSSL big numbers
+    private_key_bn   = OpenSSL::BN.new(priv, 16)
+    public_key_bn    = OpenSSL::BN.new(pub, 16)
+    public_key_point = OpenSSL::PKey::EC::Point.new(group, public_key_bn)
+
+    # Create ASN1 structure for the key
+    asn1 = OpenSSL::ASN1::Sequence(
+      [
+        OpenSSL::ASN1::Integer.new(1),                    # Version
+        OpenSSL::ASN1::OctetString(private_key_bn.to_s(2)), # Private key
+        OpenSSL::ASN1::ObjectId(CURVE, 0, :EXPLICIT),     # Curve identifier
+        OpenSSL::ASN1::BitString(                         # Public key
+          public_key_point.to_octet_string(:uncompressed),
+          1,
+          :EXPLICIT
+        )
+      ]
+    )
+
+    # Create EC key from ASN1 DER encoding
+    OpenSSL::PKey::EC.new(asn1.to_der)
+  end
+
+  # Calculates public key from private key using EC multiplication
+  # @param priv [String] Private key in hex format
+  # @return [String] Public key in hex format
+  def restore_public_key(priv)
+    # Convert private key to big number
+    private_bn = OpenSSL::BN.new priv, 16
+    group = OpenSSL::PKey::EC::Group.new CURVE
+
+    # Multiply generator point by private key to get public key point
+    public_bn = group.generator.mul(private_bn).to_bn
+    # Convert point to compressed format
+    public_bn = OpenSSL::PKey::EC::Point.new(group, public_bn).to_bn
+
+    public_bn.to_s(16).downcase
   end
 end
--- test/models/hong_bao_test.rb
+++ test/models/hong_bao_test.rb
@@ -1,6 +1,26 @@
 require "test_helper"
 
 class HongBaoTest < ActiveSupport::TestCase
+  def pkey_from_public_key(public_key)
+    group = OpenSSL::PKey::EC::Group.new("secp256k1")
+
+    public_key_bn    = OpenSSL::BN.new(public_key, 16)
+    public_key_point = OpenSSL::PKey::EC::Point.new(group, public_key_bn)
+
+    puts "public_key_bn: #{public_key_bn}"
+    puts "public_key_point: #{public_key_point}"
+    asn1 = OpenSSL::ASN1::Sequence.new(
+      [
+        OpenSSL::ASN1::Sequence.new([
+                                      OpenSSL::ASN1::ObjectId.new("id-ecPublicKey"),
+                                      OpenSSL::ASN1::ObjectId.new(group.curve_name)
+                                    ]),
+        OpenSSL::ASN1::BitString.new(public_key_point.to_octet_string(:uncompressed))
+      ]
+    )
+    OpenSSL::PKey::EC.new(asn1.to_der)
+  end
+
   test "mt pelerin test vector" do
     # Create HongBao with test vector private key
     hong_bao = HongBao.new(
@@ -9,41 +29,22 @@ class HongBaoTest < ActiveSupport::TestCase
     )
 
     hong_bao.save!
-    hong_bao.update(private_key: "4142e80a872531fd1055f52ccab713d4c7f1eee28c33415558e74faeb516de2b",
+    hong_bao.update(private_key: "bb24dd9a24ca0e8386e6ca88d85b4a4e12c563fe261c5b926e28c5033b83132c",
     mt_pelerin_request_code: "1234",
-    address: "0x270402aeB8f4dAc8203915fC26F0768feA61b532")
+    address: "mmjHdJR2ViT3way1HNFkWNuxt2rkXNJDAm")
     # Force the generation of the request
     hong_bao.send(:generate_mt_pelerin_request)
 
 
     # Assert test vector values
-    assert_equal "0x270402aeB8f4dAc8203915fC26F0768feA61b532", hong_bao.address
+    # assert_equal "0x270402aeB8f4dAc8203915fC26F0768feA61b532", hong_bao.address
     assert_equal "1234", hong_bao.mt_pelerin_request_code
-    assert_equal "/37KcpG6mEp+1oAan8/HLEvcfZFXUi6kTOxTHNjD3ZloxS8DL70v7lCmXiEyDOATm4hvewMzBO2d1n25QdJ8WBw=",
-                 hong_bao.mt_pelerin_request_hash
+  # assert_equal "MEUCIBxkej/pR81suQwMG0gZ4LLAbaUdHqaVcQwmysr3HhJEAiEA0x+BF52yvQcEXA2IK9SN53M/OWhxoTZ0lUoLUSwa0eg=",
+  #              hong_bao.mt_pelerin_request_hash
+
+
+  key = pkey_from_public_key("04e3dc626382d3d4a3e882264e2234f9536580ffd3ce568d89fe270ef0032bb7a0bf0afa1ae8bde72c44c27de12eaad8fddbc08f87832a2c4051888f0504a74bcf")
+    res = key.dsa_verify_asn1("MtPelerin-1234", Base64.decode64(hong_bao.mt_pelerin_request_hash))
+    assert res, "Signature verification failed"
   end
-
-  # test "mt pelerin test vector with seed phrase" do
-  #   # Test the second vector from docs
-  #   hong_bao = HongBao.new(
-  #     paper: papers(:one),
-  #     amount: 100,
-  #     private_key: "78ba65f1cc9427fab632340ae4d705b1485fba9f73ab5a24816907d36d5729e9"
-  #   )
-
-  #   def hong_bao.generate_mt_pelerin_request
-  #     self.mt_pelerin_request_code = "1234"
-  #     message = "MtPelerin-#{mt_pelerin_request_code}"
-  #     key = Bitcoin::Key.new(private_key, nil, false)
-  #     signature = key.sign_message(message)
-  #     self.mt_pelerin_request_hash = Base64.strict_encode64(signature)
-  #   end
-
-  #   hong_bao.send(:generate_mt_pelerin_request)
-
-  #   assert_equal "0xEa22e16EA50A43092853329F3cEEa0825Cb9B03e", hong_bao.address
-  #   assert_equal "1234", hong_bao.mt_pelerin_request_code
-  #   assert_equal "yrXNJSmMc4wvVyKEzN4cEmLTvEaridjqTULZAfMwYAMM5PgBz4fCoIWNLr5NwKhxOYiPpI2vhMlKCihWadUw5xs=",
-  #                hong_bao.mt_pelerin_request_hash
-  # end
 end
