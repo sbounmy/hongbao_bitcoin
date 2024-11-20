32fbabd316be4b704ff92f1b2ee07bee91db91e3
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2024-11-20 12:38:49 +0100

    added header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def gravatar_url(email, size = 40)
+    gravatar_id = Digest::MD5.hexdigest(email.downcase)
+    "https://gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mp"
+  end
 end
