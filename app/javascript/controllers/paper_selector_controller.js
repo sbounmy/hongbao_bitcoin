4d7f051f37b0f1859ac3508a6d21a6b628c4efda
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2024-12-03 09:38:45 +0100

    flip card

--- app/javascript/controllers/paper_selector_controller.js
+++ app/javascript/controllers/paper_selector_controller.js
@@ -1,10 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["flipCardInner"]
+
+  connect() {
+    // Initialize any necessary state or event listeners
+  }
+
+  flipCard(event) {
+    const flipCardInner = event.currentTarget.querySelector('[data-hong-bao-target="flipCardInner"]')
+    flipCardInner.classList.add('rotate-y-180')
+  }
+
+  unflipCard(event) {
+    const flipCardInner = event.currentTarget.querySelector('[data-hong-bao-target="flipCardInner"]')
+    flipCardInner.classList.remove('rotate-y-180')
+  }
+
   select(event) {
     const paperId = event.currentTarget.dataset.paperId
     document.getElementById('hong_bao_paper_id').value = paperId
     document.querySelector('form').requestSubmit()
-
   }
 }
