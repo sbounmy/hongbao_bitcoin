f91dff312731522d23c39b51d3d7003f006668e3
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2024-12-06 17:31:14 +0100

    added image processing to generate qrcode for youtube video

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Hongbao
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Configure Active Storage to use libvips
+    config.active_storage.variant_processor = :vips
   end
 end
