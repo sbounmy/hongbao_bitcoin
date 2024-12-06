87dc237b6b52311f0e32736486dcb6f23e9ebca2
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2024-12-06 13:10:12 +0100

    add date to filename

--- app/javascript/controllers/paper_pdf_controller.js
+++ app/javascript/controllers/paper_pdf_controller.js
@@ -62,14 +62,23 @@ export default class extends Controller {
     }
   }
 
+  get formattedDate() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`; // YYYY-MM-DD
+  }
+
   downloadPdf() {
     this.generatePDF().then(({ pdfUrl, pdf }) => {
-      pdf.save('Bitcoin Hong Bao.pdf')
+      const filename = `${this.formattedDate}_HongBaoBitcoin.pdf`;
+      pdf.save(filename);
     }).catch((error) => {
-      console.error('PDF download failed:', error)
+      console.error('PDF download failed:', error);
     }).finally(() => {
-      this.dispatch('pdfDownloaded')
-    })
+      this.dispatch('pdfDownloaded');
+    });
   }
 
   showPdfViewer() {
