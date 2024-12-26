fdc68ee0d8d65a24befc1f027bdc1e491d163468
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2024-12-26 17:13:08 +0100

    removed bitcoin-keypair and show test keypair

--- app/javascript/controllers/hong_bao_controller.js
+++ app/javascript/controllers/hong_bao_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import BitcoinWallet from "services/bitcoin_wallet"
 
 export default class extends Controller {
   static targets = [
@@ -19,16 +20,47 @@ export default class extends Controller {
   static values = {
     currentStep: { type: Number, default: 1 },
     currentPaper: { type: Number },
-    isTopUpStep: { type: Boolean, default: false },
-    isPdfDownloaded: { type: Boolean, default: false }
+    wallet: { type: Object, default: {} }
   }
 
   connect() {
     this.setupRefreshWarning()
-    this.isPdfDownloaded = false
-    document.addEventListener('paper-pdf:pdfDownloaded', () => {
-      this.isPdfDownloaded = true
-    })
+    this.generateWallet()
+  }
+
+  async generateWallet() {
+    const wallet = BitcoinWallet.generate()
+    const key = wallet.nodePathFor("m/44'/0'/0'/0/0")
+
+    // Generate Mt Pelerin request if needed
+    const mtPelerin = await wallet.generateMtPelerinRequest()
+
+    this.walletValue = {
+      address: key.address,
+      privateKey: key.privateKey,
+      mnemonic: wallet.mnemonic,
+      addressQrcode: await key.addressQrcode(),
+      privateKeyQrcode: await key.privateKeyQrcode(),
+      publicKeyQrcode: await key.publicKeyQrcode(),
+      mtPelerinCode: mtPelerin.requestCode,
+      mtPelerinHash: mtPelerin.requestHash
+    }
+  }
+
+  walletValueChanged() {
+    console.log('hasMnemonicTarget:', this.hasMnemonicTarget)
+    if (this.hasMnemonicTarget) {
+      this.mnemonicTarget.value = this.walletValue.mnemonic
+    }
+    if (this.hasPrivateKeyTarget) {
+      this.privateKeyTarget.value = this.walletValue.privateKey
+    }
+    if (this.hasAddressTarget) {
+      this.addressTarget.value = this.walletValue.address
+    }
+
+    // Dispatch event for other controllers
+    this.dispatch("walletChanged", { detail: this.walletValue })
   }
 
   setupRefreshWarning() {
