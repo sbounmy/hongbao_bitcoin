0cc12792797258aea7dd72a07b5caf262741f3a2
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-02-26 13:24:48 +0100

    fix gemfilelock and faceswap namespace

--- app/controllers/webhooks/face_swap_controller.rb
+++ app/controllers/webhooks/face_swap_controller.rb
@@ -1,12 +1,14 @@
-class FaceSwapController < ApplicationController
-  skip_before_action :verify_authenticity_token
+module Webhooks
+  class FaceSwapController < ApplicationController
+    skip_before_action :verify_authenticity_token
 
-  def callback
-    data = params.permit(:success, :type, :task_id, :result_image)
+    def callback
+      data = params.permit(:success, :type, :task_id, :result_image)
 
-    # Traite les données (ex : enregistrer le résultat en base)
-    Rails.logger.info "Face swap terminé : #{data.to_h}"
+      # Traite les données (ex : enregistrer le résultat en base)
+      Rails.logger.info "Face swap terminé : #{data.to_h}"
 
-    head :ok
+      head :ok
+    end
   end
 end
