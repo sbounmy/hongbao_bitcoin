9dc67b9373326a92a1896c3173130e6175832fa7
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-03-10 23:18:21 +0100

    removed n+1 for papers image attachment

--- app/controllers/hong_baos_controller.rb
+++ app/controllers/hong_baos_controller.rb
@@ -17,7 +17,7 @@ class HongBaosController < ApplicationController
 
   def new
     @hong_bao = HongBao.new(paper_id: params[:paper_id])
-    @papers = Paper.active.template
+    @papers = Paper.active.template.with_attached_image_front.with_attached_image_back
     @payment_methods = PaymentMethod.active
     @current_step = (params[:step] || 1).to_i
 
