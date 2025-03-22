4f69dee2cd489799e3b337c172d5929083133e38
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-03-22 08:57:38 +0100

    refactor spec

--- spec/lib/client/request_spec.rb
+++ spec/lib/client/request_spec.rb
@@ -4,48 +4,51 @@ require 'webmock/rspec'
 RSpec.describe Client::Request do
   let(:base_url) { "https://api.example.com" }
 
+  def get(url, **params)
+    described_class.new(:get, url, **params)
+  end
+
+  def post(url, **params)
+    described_class.new(:post, url, **params)
+  end
+
   describe "#execute" do
     context "with GET requests" do
+      let(:url) { "#{base_url}/users" }
+
       it "handles simple GET requests" do
-        request = described_class.new(:get, "#{base_url}/users")
-        stub_request(:get, "#{base_url}/users")
+        stub_request(:get, url)
           .to_return(status: 200, body: { users: [] }.to_json)
 
-        response = request.execute
+        response = get(url).execute
         expect(response).to be_a(Net::HTTPSuccess)
-        expect(WebMock).to have_requested(:get, "#{base_url}/users")
+        expect(WebMock).to have_requested(:get, url)
           .with(headers: { "Content-Type" => "application/json" })
       end
 
       it "properly encodes query parameters" do
-        request = described_class.new(
-          :get,
-          "#{base_url}/users",
-          limit: 10,
-          offset: 20
-        )
-
-        stub_request(:get, "#{base_url}/users")
+        stub_request(:get, url)
           .with(query: { limit: "10", offset: "20" })
           .to_return(status: 200, body: { users: [] }.to_json)
 
-        request.execute
-        expect(WebMock).to have_requested(:get, "#{base_url}/users")
+        get(url, limit: 10, offset: 20).execute
+        expect(WebMock).to have_requested(:get, url)
           .with(query: { limit: "10", offset: "20" })
       end
     end
 
     context "with POST requests" do
+      let(:url) { "#{base_url}/users" }
+
       it "handles JSON POST requests" do
         data = { name: "John", email: "john@example.com" }
-        request = described_class.new(:post, "#{base_url}/users", **data)
 
-        stub_request(:post, "#{base_url}/users")
+        stub_request(:post, url)
           .with(body: data)
           .to_return(status: 201, body: data.merge(id: 1).to_json)
 
-        request.execute
-        expect(WebMock).to have_requested(:post, "#{base_url}/users")
+        post(url, **data).execute
+        expect(WebMock).to have_requested(:post, url)
           .with(
             body: data,
             headers: { "Content-Type" => "application/json" }
@@ -53,19 +56,18 @@ RSpec.describe Client::Request do
       end
 
       it "handles multipart form data with files" do
+        url = "#{base_url}/upload"
         file = double("ActiveStorage::Blob")
         allow(file).to receive(:download).and_return("file content")
 
-        request = described_class.new(
-          :post,
-          "#{base_url}/upload",
-          content_type: Client::Request::CONTENT_TYPES[:MULTIPART],
+        request = post(
+          url,
+          content_type: 'multipart/form-data',
           files: { avatar: file },
           name: "John"
         )
 
-        puts "request: #{request.inspect}"
-        stub_request(:post, "#{base_url}/upload")
+        stub_request(:post, url)
           .with(
             headers: { 'Content-Type' => /multipart\/form-data/ }
           )
@@ -75,7 +77,7 @@ RSpec.describe Client::Request do
 
         expect(response.code).to eq("201")
 
-        expect(WebMock).to have_requested(:post, "#{base_url}/upload")
+        expect(WebMock).to have_requested(:post, url)
           .with(
             headers: { 'Content-Type' => /multipart\/form-data/ },
           )
@@ -83,14 +85,16 @@ RSpec.describe Client::Request do
     end
 
     context "with authorization" do
+      let(:url) { "#{base_url}/secure" }
       it "adds authorization header when api_key is provided" do
-        request = described_class.new(:get, "#{base_url}/secure")
-        stub_request(:get, "#{base_url}/secure")
+        request = get(url)
+
+        stub_request(:get, url)
           .with(headers: { "Authorization" => "Bearer test_key" })
           .to_return(status: 200, body: "{}")
 
-        request.execute(api_key: "test_key")
-        expect(WebMock).to have_requested(:get, "#{base_url}/secure")
+        get(url).execute(api_key: "test_key")
+        expect(WebMock).to have_requested(:get, url)
           .with(headers: {
             "Authorization" => "Bearer test_key",
             "Content-Type" => "application/json"
@@ -99,19 +103,16 @@ RSpec.describe Client::Request do
     end
 
     context "with custom headers" do
+      let(:url) { "#{base_url}/users" }
       it "allows adding custom headers" do
-        request = described_class.new(
-          :get,
-          "#{base_url}/users",
-          headers: { "X-Custom" => "value" }
-        )
+        request = get(url, headers: { "X-Custom" => "value" })
 
-        stub_request(:get, "#{base_url}/users")
+        stub_request(:get, url)
           .with(headers: { "X-Custom" => "value" })
           .to_return(status: 200, body: "{}")
 
-        request.execute
-        expect(WebMock).to have_requested(:get, "#{base_url}/users")
+        get(url, headers: { "X-Custom" => "value" }).execute
+        expect(WebMock).to have_requested(:get, url)
           .with(headers: {
             "X-Custom" => "value",
             "Content-Type" => "application/json"
@@ -120,14 +121,15 @@ RSpec.describe Client::Request do
     end
 
     context "with different HTTP methods" do
+      let(:url) { "#{base_url}/users/1" }
       {
         put: Net::HTTP::Put,
         patch: Net::HTTP::Patch,
         delete: Net::HTTP::Delete
       }.each do |method, klass|
         it "handles #{method.upcase} requests" do
-          request = described_class.new(method, "#{base_url}/users/1")
-          stub_request(method, "#{base_url}/users/1")
+          request = described_class.new(method, url)
+          stub_request(method, url)
             .to_return(status: 200, body: "{}")
 
           response = request.execute
@@ -145,21 +147,21 @@ RSpec.describe Client::Request do
 
     context "with SSL" do
       it "uses SSL for https URLs" do
-        request = described_class.new(:get, "https://api.example.com/users")
-        stub_request(:get, "https://api.example.com/users")
+        url = "https://api.example.com/users"
+        stub_request(:get, url)
           .to_return(status: 200, body: "{}")
 
-        request.execute
-        expect(WebMock).to have_requested(:get, "https://api.example.com/users")
+        get(url).execute
+        expect(WebMock).to have_requested(:get, url)
       end
 
       it "doesn't use SSL for http URLs" do
-        request = described_class.new(:get, "http://api.example.com/users")
-        stub_request(:get, "http://api.example.com/users")
+        url = "http://api.example.com/users"
+        stub_request(:get, url)
           .to_return(status: 200, body: "{}")
 
-        request.execute
-        expect(WebMock).to have_requested(:get, "http://api.example.com/users")
+        get(url).execute
+        expect(WebMock).to have_requested(:get, url)
       end
     end
   end
