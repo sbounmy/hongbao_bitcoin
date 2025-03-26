9b74722139744e734f27c074bab25cdd596be41b
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-03-26 16:00:55 +0000

    chore: change all old InstagramService#fetch_media calls to Client::Instagram#me_media

---
+++ lib/client/instagram.rb
@@ -0,0 +1,20 @@
+module Client
+  class Instagram < Base
+    PARAMS = { fields: "id,caption,media_type,media_url,permalink,thumbnail_url,timestamp", access_token: Rails.application.credentials.instagram[:token] }
+
+    url "https://graph.instagram.com"
+
+    get "/me/media",
+      as: :fetch,
+      content_type: "multipart/form-data",
+      key: "data"
+
+    def me_media
+      fetch(**PARAMS)
+    end
+
+    def api_key_credential_path
+      ""
+    end
+  end
+end
