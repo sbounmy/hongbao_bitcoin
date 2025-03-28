78e47169f290b80fc1d3d2bc95a1a612230120b1
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-03-28 10:15:26 +0100

    bin/dev not excuting test server

--- playwright.config.js
+++ playwright.config.js
@@ -76,11 +76,11 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'foreman start -f Procfile.test',
+    port: 3003,
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000, // 2 minutes for slow Rails boot
+  },
 });
 
