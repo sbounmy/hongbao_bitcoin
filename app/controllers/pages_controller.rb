c140f175879320bf9ec029530de49f3a4cf24dc0
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-04-03 15:47:15 +0200

    add sections to /v2 + refactor octocat to view helper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,6 @@ class PagesController < ApplicationController
     # Will be used to list available styles and papers
     @styles = Ai::Style.all
     @papers = Paper.all
+    @instagram_posts = cache("instagram_posts", expires_in: 2.hour) { InstagramService.new.fetch_media }
   end
 end
