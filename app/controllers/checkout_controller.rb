093499663903ce795ea64bcab7e4b3ffa382dfec
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-04-14 21:48:31 +0200

    refactor checkout create to service

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,9 +2,13 @@ class CheckoutController < ApplicationController
   allow_unauthenticated_access
   def create
     # Create a Stripe Checkout Session
-    session = Stripe::Checkout::Session.create(checkout_params)
-    # Redirect to Stripe Checkout
-    redirect_to session.url, allow_other_host: true
+    result = Checkout::Create.call(params, current_user:)
+    if result.success?
+      redirect_to result.payload.url, allow_other_host: true
+    else
+      flash[:alert] = "Payment failed. Please try again."
+      redirect_to root_path
+    end
   end
 
   def success
@@ -33,25 +37,5 @@ class CheckoutController < ApplicationController
     # Handle cancelled payment
     flash[:alert] = "Payment cancelled."
     redirect_to root_path
-  end
-
-  private
-
-  def checkout_params
-    p = {
-      payment_method_types: [ "card" ],
-      line_items: [ {
-        price: params[:price_id],
-        quantity: 1
-      } ],
-      mode: "payment",
-      success_url: CGI.unescape(success_checkout_index_url(session_id: "{CHECKOUT_SESSION_ID}")), # so {CHECKOUT_SESSION_ID} is not escaped
-      cancel_url: cancel_checkout_index_url
-    }
-    if authenticated?
-      p[:customer] = current_user.stripe_customer_id if current_user.stripe_customer_id
-      p[:customer_email] = current_user.email
-    end
-    p
   end
 end
