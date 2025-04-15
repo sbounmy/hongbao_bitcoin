838c062ae4ff4d09814bb369cce8470bcbf87ce2
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-04-15 10:34:19 +0200

    fix checkout for usa

--- e2e/playwright/e2e/checkout.spec.js
+++ e2e/playwright/e2e/checkout.spec.js
@@ -5,6 +5,17 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+async function checkout(page, email) {
+  await page.fill('input[name="email"]', email);
+  await page.fill('input[name="cardNumber"]', '4242424242424242');
+  await page.fill('input[name="cardExpiry"]', '12/2034');
+  await page.fill('input[name="cardCvc"]', '123');
+  await page.fill('input[name="billingName"]', email.split('@')[0]);
+  await page.selectOption('select[name="billingCountry"]', 'United States');
+  await page.fill('input[name="billingPostalCode"]', '12345');
+  await page.click('button[type="submit"]');
+  await expect(page.getByText('Processing...')).toBeVisible();
+}
 test.describe('Stripe Checkout Flow', () => {
 
   test('visitor can buy tokens and become signed up as a user', async ({ page }) => {
@@ -19,13 +30,7 @@ test.describe('Stripe Checkout Flow', () => {
     expect(page.url()).toContain('checkout.stripe.com');
 
     const random = getRandomInt(9999);
-    await page.fill('input[name="email"]', `hongbaob+${random}@example.com`);
-    await page.fill('input[name="cardNumber"]', '4242424242424242');
-    await page.fill('input[name="cardExpiry"]', '12/2034');
-    await page.fill('input[name="cardCvc"]', '123');
-    await page.fill('input[name="billingName"]', `Satoshi Nakamoto ${random}`);
-    await page.click('button[type="submit"]');
-    await expect(page.getByText('Processing...')).toBeVisible();
+    await checkout(page, `hongbaob+${random}@example.com`);
     // expect(page.getByText('Processing...')).toBeHidden({ timeout: 5_000 });
     // expect(page.url()).toBe(page.url('/'));
     await page.waitForTimeout(5_000);
@@ -45,13 +50,7 @@ test.describe('Stripe Checkout Flow', () => {
     expect(page.url()).toContain('checkout.stripe.com');
 
     const random = getRandomInt(9999);
-    await page.fill('input[name="email"]', `satoshi@example.com`);
-    await page.fill('input[name="cardNumber"]', '4242424242424242');
-    await page.fill('input[name="cardExpiry"]', '12/2034');
-    await page.fill('input[name="cardCvc"]', '123');
-    await page.fill('input[name="billingName"]', `Satoshi Nakamoto ${random}`);
-    await page.click('button[type="submit"]');
-    await expect(page.getByText('Processing...')).toBeVisible();
+    await checkout(page, `satoshi@example.com`);
     // expect(page.getByText('Processing...')).toBeHidden({ timeout: 5_000 });
     // expect(page.url()).toBe(page.url('/'));
     await page.waitForTimeout(5_000);
@@ -82,6 +81,8 @@ test.describe('Stripe Checkout Flow', () => {
     await page.fill('input[name="cardExpiry"]', '12/2034');
     await page.fill('input[name="cardCvc"]', '123');
     await page.fill('input[name="billingName"]', `Satoshi Nakamoto ${random}`);
+    await page.selectOption('select[name="billingCountry"]', 'United States');
+    await page.fill('input[name="billingPostalCode"]', '12345');
     await page.click('button[type="submit"]');
     await expect(page.getByText('Processing...')).toBeVisible();
     // expect(page.getByText('Processing...')).toBeHidden({ timeout: 5_000 });
