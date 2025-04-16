86948710c02cbd572912ca0759ef4b04fbf21420
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-04-16 11:22:38 +0200

    added drawer instead of dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,4 @@
 module ApplicationHelper
-  def gravatar_url(email, size = 40)
-    gravatar_id = Digest::MD5.hexdigest(email.downcase)
-    "https://gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mp"
-  end
-
   def render_payment_logo(logo)
     return placeholder_logo unless logo.attached?
 
