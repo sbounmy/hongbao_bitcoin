b37c399edcfb720ee350f3eaed779e86a447229b
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-04-23 14:59:29 +0200

    fix missing @themes

--- app/controllers/hong_baos_controller.rb
+++ app/controllers/hong_baos_controller.rb
@@ -2,6 +2,7 @@ class HongBaosController < ApplicationController
   allow_unauthenticated_access only: %i[new show index search]
 
   def index
+    @themes = Ai::Theme.all
     # Just render the QR scanner view
   end
 
@@ -31,6 +32,7 @@ class HongBaosController < ApplicationController
   def show
     @hong_bao = HongBao.from_scan(params[:id])
     @payment_methods = PaymentMethod.active
+    @themes = Ai::Theme.all
     @current_step = (params[:step] || 1).to_i
     @steps = Step.for_show
   end
