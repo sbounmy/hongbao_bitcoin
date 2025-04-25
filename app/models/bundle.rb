7e357cb49d07803a7368ede2163e5a3ccd978d53
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-04-25 10:18:37 +0200

    started spec bundle

--- app/models/bundle.rb
+++ app/models/bundle.rb
@@ -1,3 +1,7 @@
+# Represents a collection of user-selected inputs (e.g., styles, themes)
+# chosen for a specific generation task or request. It groups the associated
+# `Input` records via the `InputItem` join table.
+# Each input style with theme will be passed as a chat to the AI
 class Bundle < ApplicationRecord
   belongs_to :user
 
---
+++ e2e/playwright/e2e/bundle.spec.js
@@ -0,0 +1,32 @@
+const { test, expect } = require('../support/test-setup');
+const { appVcrInsertCassette, forceLogin } = require('../support/on-rails');
+
+test.describe('Bundle generation', () => {
+
+  test.beforeEach(async ({ page }) => {
+    // Create admin user and authenticate
+    await forceLogin(page, {
+      email: 'satoshi@example.com'
+    });
+  });
+
+  test('user can create a bundle', async ({ page }) => {
+    await appVcrInsertCassette('bundle')
+
+     // Select styles
+    await page.getByText('Ghibli').filter({ visible: true }).first().click({ force: true });
+    await page.getByText('Marvel').filter({ visible: true }).first().click({ force: true });
+
+    // Upload image
+    await page.locator('#file-upload').setInputFiles('spec/fixtures/files/satoshi.jpg');
+    const count = await page.locator('#main-content .papers-item-component').count();
+
+    await expect(page.getByText('Processing...')).toBeHidden();
+    await page.getByRole('button', { name: 'Generate' }).click();
+    await expect(page.getByText('Processing...')).toBeVisible();
+    await expect(page.getByText('Processing...')).toBeHidden();
+    // this should be done through turbo frame
+    await page.goto('/');
+    await expect(page.locator('#main-content .papers-item-component')).toHaveCount(count + 2);
+  });
+});
