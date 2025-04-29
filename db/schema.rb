7395c3bf94be610ba4c918f758d54a6bb05b9a5e
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-04-29 16:30:48 +0200

    removed null false for metadata

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_29_095023) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_29_142835) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -154,7 +154,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_29_095023) do
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.json "metadata", default: "{}", null: false
+    t.json "metadata", default: "{}"
   end
 
   create_table "messages", force: :cascade do |t|
