0ffa6e294673f027826a7bcd7ec2ce64b7278e41
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-05-05 22:35:49 +0200

    added bundle e2e and fix app('perform_jobs') for e2e

--- e2e/playwright/e2e/bundle.spec.js
+++ e2e/playwright/e2e/bundle.spec.js
@@ -5,15 +5,15 @@ test.describe('Bundle generation', () => {
 
   test.beforeEach(async ({ page }) => {
     // Create admin user and authenticate
-    await appVcrInsertCassette('bundle')
+    await appVcrInsertCassette('bundle', { serialize_with: 'compressed' })
     await forceLogin(page, {
       email: 'satoshi@example.com'
     });
   });
 
   test('user can create a bundle', async ({ page }) => {
-    test.skip('until we find out how to perform jobs')
-    test.setTimeout(1_200_000); // slow test
+    // test.skip('until we find out how to perform jobs')
+    // test.setTimeout(1_200_000); // slow test
 
      // Select styles
     await page.getByText('Ghibli').filter({ visible: true }).first().click({ force: true });
@@ -29,7 +29,7 @@ test.describe('Bundle generation', () => {
     await expect(page.getByText('Processing...')).toBeHidden();
     await app('perform_jobs');
     // this should be done through turbo frame
-    await page.waitForTimeout(700_000); // jobs takes around chatgpt API 60s
+    // await page.waitForTimeout(700_000); // jobs takes around chatgpt API 60s
     await page.goto('/');
     await expect(page.locator('#main-content .papers-item-component')).toHaveCount(count + 2);
   });
