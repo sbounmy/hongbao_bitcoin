5dd6d3734fe2836d10f7001c40ea87b448330fdb
Salma <salma.talbi@tadeco-group.tn>
2025-05-06 14:40:47 +0100

    fix lint

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Paper, type: :model do
   it 'sets elements from theme' do
     @paper.save!
     expect(@paper.elements).to eq(inputs(:dollar).ai)
-    expect(@paper.elements.keys).to include("private_key_qrcode", "private_key_text", "public_address_qrcode", "public_address_text", "mnemonic_text","custom_text")
+    expect(@paper.elements.keys).to include("private_key_qrcode", "private_key_text", "public_address_qrcode", "public_address_text", "mnemonic_text", "custom_text")
   end
 
   it 'sets elements from default elements when theme is not set' do
@@ -19,6 +19,4 @@ RSpec.describe Paper, type: :model do
     expect(@paper.elements).to eq(Input::Theme.default_ai_elements)
     expect(@paper.elements.keys).to include("private_key_qrcode", "private_key_text", "public_address_qrcode", "public_address_text", "mnemonic_text")
   end
-
-
 end
