5de8b64bc8fe10544315b7a7860b5c68f798feaf
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-05-08 08:04:23 +0200

    overwrite later method too

--- spec/support/turbo_streams_channel.rb
+++ spec/support/turbo_streams_channel.rb
@@ -3,8 +3,9 @@
 # This fix rspec assets precompile by skipping turbo broadcasting (we do test then on e2e)
 # ActionView::Template::Error: The asset 'tailwind.css' was not found in the load path.
 # https://dev.to/edwinthinks/skip-turbo-broadcasting-to-speed-up-seeding-process-in-rails-33cp
-class Turbo::StreamsChannel
-  [ :broadcast_append_to, :broadcast_prepend_to, :broadcast_replace_to, :broadcast_remove_to ].each do |method|
+Turbo::StreamsChannel.class_eval do
+  [ :broadcast_append_to, :broadcast_prepend_to, :broadcast_replace_to, :broadcast_remove_to,
+    :broadcast_append_later_to, :broadcast_prepend_later_to, :broadcast_replace_later_to, :broadcast_remove_later_to ].each do |method|
     define_singleton_method(method) do |*args|
       nil
     end
