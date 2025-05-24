08173825ded32d6018f6290a83cec467d8bcf090
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-05-24 14:56:03 +0200

    review with grid and better card component

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -1,4 +1,24 @@
 class CardComponent < ApplicationComponent
-  renders_one :back
-  renders_one :front
+  renders_one :back, "CardBackComponent"
+  renders_one :front, "CardFrontComponent"
+
+  attr_reader :options
+
+  def initialize(options = {})
+    @options = options
+  end
+
+  class CardBodyComponent < ApplicationComponent
+    attr_reader :options
+
+    def initialize(options = {})
+      @options = options
+    end
+  end
+
+  class CardFrontComponent < CardBodyComponent
+  end
+
+  class CardBackComponent < CardBodyComponent
+  end
 end
