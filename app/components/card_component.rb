f7f176dde506361323f452f7b359b2733e3e145f
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-05-24 09:33:20 +0200

    fix gradient and card params

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -1,9 +1,4 @@
 class CardComponent < ApplicationComponent
   renders_one :back
   renders_one :front
-
-  def initialize(title:, description:)
-    @title = title
-    @description = description
-  end
 end
