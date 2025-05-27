51de3134c8401285707fb25efa7bed51ec68212d
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-05-27 07:08:09 +0200

    new og image

--- app/controllers/og_image_controller.rb
+++ app/controllers/og_image_controller.rb
@@ -1,10 +1,11 @@
 class OgImageController < ApplicationController
+  allow_unauthenticated_access
   # Skip any authentication if you have it
 
   # Skip CSRF protection as this is just for viewing
   skip_before_action :verify_authenticity_token
 
-  layout false # Don't use the application layout
+  # layout false # Don't use the application layout
 
   def show
     # Set response headers for better caching
