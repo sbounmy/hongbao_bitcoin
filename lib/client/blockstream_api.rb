b0576f93ad6368fef3dc3c12aadfde360e3cf1e1
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-06-07 08:30:43 +0200

    fix spec and removed unused endpoints

--- lib/client/blockstream_api.rb
+++ lib/client/blockstream_api.rb
@@ -14,13 +14,10 @@ module Client
     }
 
     # Address endpoints - core functionality for Balance model
-    get "/address/:address", as: :get_address
     get "/address/:address/txs", as: :get_address_transactions
     get "/address/:address/utxo", as: :get_address_utxos
 
     # Transaction endpoints - needed for transaction details in balance view
-    get "/tx/:txid", as: :get_transaction
-    get "/tx/:txid/status", as: :get_transaction_status
     get "/tx/:txid/hex", as: :get_transaction_hex
 
     # Block endpoints - needed for confirmations calculation
--- spec/lib/client/blockstream_api_spec.rb
+++ spec/lib/client/blockstream_api_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe Client::BlockstreamApi do
   let(:sample_txid) { "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b" }
 
   describe "initialization" do
-    context "mainnet" do
+    context "mainnet"  do
       let(:client) { Client::BlockstreamApi.new(dev: false) }
 
-      it "sets the correct base URL for mainnet" do
+      it "sets the correct base URL for mainnet", vcr: { cassette_name: "blockstream_api/token" } do
         expect(client.base_url).to eq("https://blockstream.info/api")
       end
     end
@@ -16,7 +16,7 @@ RSpec.describe Client::BlockstreamApi do
     context "testnet" do
       let(:client) { Client::BlockstreamApi.new(dev: true) }
 
-      it "sets the correct base URL for testnet" do
+      it "sets the correct base URL for testnet", vcr: { cassette_name: "blockstream_api/token_dev" } do
         expect(client.base_url).to eq("https://blockstream.info/testnet/api")
       end
     end
@@ -24,17 +24,6 @@ RSpec.describe Client::BlockstreamApi do
 
   describe "address endpoints" do
     let(:client) { Client::BlockstreamApi.new(dev: false) }
-
-    context "get_address", vcr: { cassette_name: "blockstream_api/get_address" } do
-      it "fetches address information" do
-        address_info = client.get_address(sample_address)
-
-        expect(address_info.address).to eq(sample_address)
-        expect(address_info.chain_stats).to be_a(Client::Object)
-        expect(address_info.chain_stats.funded_txo_count).to be_a(Integer)
-        expect(address_info.chain_stats.funded_txo_sum).to be_a(Integer)
-      end
-    end
 
     context "get_address_transactions", vcr: { cassette_name: "blockstream_api/get_address_transactions" } do
       it "fetches address transactions as a list" do
@@ -64,26 +53,11 @@ RSpec.describe Client::BlockstreamApi do
   describe "transaction endpoints" do
     let(:client) { Client::BlockstreamApi.new(testnet: false) }
 
-    context "get_transaction", vcr: { cassette_name: "blockstream_api/get_transaction" } do
+    context "get_transaction_hex", vcr: { cassette_name: "blockstream_api/get_transaction_hex" } do
       it "fetches transaction details" do
-        transaction = client.get_transaction(sample_txid)
+        transaction = client.get_transaction_hex(sample_txid)
 
-        expect(transaction.txid).to eq(sample_txid)
-        expect(transaction.vin).to be_a(Array)
-        expect(transaction.vin[0]).to be_a(Client::Object)
-        expect(transaction.vout).to be_a(Array)
-        expect(transaction.vout[0].value).to be_a(Integer)
-        expect(transaction.status).to be_a(Client::Object)
-      end
-    end
-
-    context "get_transaction_status", vcr: { cassette_name: "blockstream_api/get_transaction_status" } do
-      it "fetches transaction status" do
-        status = client.get_transaction_status(sample_txid)
-
-        expect(status.confirmed).to be_a(TrueClass).or be_a(FalseClass)
-        expect(status.block_height).to be_a(Integer) if status.confirmed
-        expect(status.block_hash).to be_a(String) if status.confirmed
+        expect(transaction).to eql('01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000')
       end
     end
   end
@@ -95,8 +69,8 @@ RSpec.describe Client::BlockstreamApi do
       it "fetches current tip height" do
         height = client.get_tip_height
 
-        expect(height).to match(/^\d+$/)
-        expect(height.to_i).to be > 800000
+        expect(height.to_s).to match(/^\d+$/)
+        expect(height).to be > 800000
       end
     end
   end
