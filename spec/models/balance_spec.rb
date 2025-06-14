7dce2de58e69d51961dbdbe27680264ade8c5dd4
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-06-14 08:50:44 +0200

    fix spec

--- spec/models/balance_spec.rb
+++ spec/models/balance_spec.rb
@@ -20,13 +20,20 @@ describe Balance do
   end
 
   describe "#utxos_for_transaction", vcr: { cassette_name: "balance/utxos_for_transaction" } do
-    it "returns the utxos for the transaction" do
-      expect(balance.utxos_for_transaction).to be_an(Array)
-      puts balance.utxos_for_transaction.inspect
-      utxo = balance.utxos_for_transaction.last
+    it "returns full utxos with true" do
+      expect(balance.utxos_for_transaction(true)).to be_an(Array)
+      utxo = balance.utxos_for_transaction(true).last
       expect(utxo[:txid]).to eq("8aaa32840db9a9c5ace5283e50cf12d7394f7a2d44efeb97f202f95d453b2aff")
       expect(utxo[:vout]).to eq(0)
       expect(utxo[:script]).to eq('0014cedb7d9d04af255c704325f9271a4a92e9d18055')
+    end
+
+    it "returns utxos with false" do
+      expect(balance.utxos_for_transaction).to be_an(Array)
+      utxo = balance.utxos_for_transaction.last
+      expect(utxo[:txid]).to eq("8aaa32840db9a9c5ace5283e50cf12d7394f7a2d44efeb97f202f95d453b2aff")
+      expect(utxo[:vout]).to eq(0)
+      expect(utxo[:script]).to be_blank
     end
   end
 end
