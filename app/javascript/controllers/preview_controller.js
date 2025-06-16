ed1458f92ffd72a28403602c27d10c037ad1f462
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-06-16 14:46:51 +0100

    fix: check if a file is loaded, and reload it back to preview.src

--- app/javascript/controllers/preview_controller.js
+++ app/javascript/controllers/preview_controller.js
@@ -3,7 +3,25 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="previews"
 export default class extends Controller {
   static targets = ["input", "preview"];
+  connect() {
+    if (this.inputTarget.files && this.inputTarget.files[0]) {
+      let input = this.inputTarget;
+      let preview = this.previewTarget;
+      let file = input.files[0];
 
+      if (file) {
+        let reader = new FileReader();
+        reader.onloadend = () => {
+          preview.src = reader.result;
+        };
+        reader.readAsDataURL(file);
+        this.dispatch("selected", { detail: { file } });
+      } else {
+        preview.src = "";
+        this.dispatch("none", { detail: {} });
+      }
+    }
+  }
   preview() {
     // TODO: this
     console.log("preview");
