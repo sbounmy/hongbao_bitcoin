2ad81f1956a91bd636a45a923a0e9e5e29151ce7
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-06-17 14:34:21 +0100

    chore: make stimulus controller preview dry

--- app/javascript/controllers/preview_controller.js
+++ app/javascript/controllers/preview_controller.js
@@ -3,42 +3,27 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="previews"
 export default class extends Controller {
   static targets = ["input", "preview"];
+  
   connect() {
-    if (this.inputTarget.files && this.inputTarget.files[0]) {
-      let input = this.inputTarget;
-      let preview = this.previewTarget;
-      let file = input.files[0];
-
-      if (file) {
-        let reader = new FileReader();
-        reader.onloadend = () => {
-          preview.src = reader.result;
-        };
-        reader.readAsDataURL(file);
-        this.dispatch("selected", { detail: { file } });
-      } else {
-        preview.src = "";
-        this.dispatch("none", { detail: {} });
-      }
-    }
+    this.updatePreview();
   }
+  
   preview() {
-    // TODO: this
-    console.log("preview");
-    let input = this.inputTarget;
-    let preview = this.previewTarget;
-    let file = input.files[0];
-    let reader = new FileReader();
+    this.updatePreview();
+  }
 
-    reader.onloadend = function () {
-      preview.src = reader.result;
-    };
-
+  updatePreview() {
+    const file = this.inputTarget.files?.[0];
+    
     if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        this.previewTarget.src = reader.result;
+      };
       reader.readAsDataURL(file);
       this.dispatch("selected", { detail: { file } });
     } else {
-      preview.src = "";
+      this.previewTarget.src = "";
       this.dispatch("none", { detail: {} });
     }
   }
