402392c53d7a0e40ecb895a8ca94afa4fa4eb6b4
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-06-19 13:52:44 +0100

    feat: attach front image to Input::Theme model

--- app/models/input/theme.rb
+++ app/models/input/theme.rb
@@ -99,6 +99,7 @@ class Input::Theme < Input
 
   has_one_attached :hero_image
   has_one_attached :back_image
+  has_one_attached :front_image
 
   validates :ui_name, presence: true
   validates :slug, presence: true, uniqueness: true
