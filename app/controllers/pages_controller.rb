48f631d3660dbb7cfbc0e9aee73c4114fcd34d26
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-06-25 15:17:41 +0200

    fix : limit to reduce page size

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,7 @@ class PagesController < ApplicationController
   def v2
     # Will be used to list available styles and papers
     @styles = Input::Style.with_attached_image
-    @papers = Paper.active.recent.with_attached_image_front.with_attached_image_back
+    @papers = Paper.active.recent.with_attached_image_front.with_attached_image_back.limit(5)
     @bundle = Bundle.new
     @bundle.input_items.build(input: Input::Theme.first)
     @instagram_posts = cache("instagram_posts", expires_in: 2.hour) { InstagramService.new.fetch_media }
