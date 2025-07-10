45b2d82f1513db863a42b33c3c4e568827aad1f0
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-07-10 11:37:30 +0100

    feat: add LineItems model

---
+++ db/migrate/20250710103448_create_line_items.rb
@@ -0,0 +1,14 @@
+class CreateLineItems < ActiveRecord::Migration[8.0]
+  def change
+    create_table :line_items do |t|
+      t.references :order, null: false, foreign_key: true
+      t.integer :quantity, null: false
+      t.decimal :price, null: false
+      t.string :currency, null: false
+      t.string :stripe_price_id
+      t.json :metadata, default: {}
+
+      t.timestamps
+    end
+  end
+end
