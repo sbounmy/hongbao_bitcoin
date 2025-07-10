cfdf7a94ec76fee57bafb3115a81ba2b85c0f67a
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-07-10 11:41:30 +0100

    feat: add enum for status & payment provider, relations & validation in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,19 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :line_items, dependent: :destroy
+  has_many :tokens, dependent: :destroy
+
+  enum status: {
+    pending: "pending",
+    completed: "completed",
+    failed: "failed"
+  }
+
+  enum payment_provider: {
+    stripe: "stripe",
+    btcpay: "btcpay"
+  }
+
+  # external_id is the unique identifier for the transaction in the payment gateway
+  validates :external_id, presence: true, uniqueness: true
 end
