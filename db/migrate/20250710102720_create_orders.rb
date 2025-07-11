069be8a85e1e8ef75e6de33409d1868e0332eed3
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-07-11 10:39:05 +0100

    feat: remove status enum & column from Order to add aasm state (rollbacked database)

--- db/migrate/20250710102720_create_orders.rb
+++ db/migrate/20250710102720_create_orders.rb
@@ -2,7 +2,6 @@ class CreateOrders < ActiveRecord::Migration[8.0]
   def change
     create_table :orders do |t|
       t.references :user, null: true, foreign_key: true
-      t.string :status, default: "pending", null: false
       t.string :payment_provider, null: false
       t.decimal :total_amount, null: false
       t.string :currency, null: false
