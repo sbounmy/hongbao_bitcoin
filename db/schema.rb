7472a0226b5cbd18bb3f879d21f1d9f2b1dbeec1
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-07-11 10:41:52 +0100

    feat: migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_10_103448) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_11_094049) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -192,6 +192,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_10_103448) do
     t.string "external_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "aasm_state"
     t.index ["external_id"], name: "index_orders_on_external_id", unique: true
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
