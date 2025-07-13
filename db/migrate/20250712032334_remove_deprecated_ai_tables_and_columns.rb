ad19b60cfd0d001d4a5bb8f52b2340eff773abd0
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-07-13 18:28:36 +0200

    fix: drop if exists

--- db/migrate/20250712032334_remove_deprecated_ai_tables_and_columns.rb
+++ db/migrate/20250712032334_remove_deprecated_ai_tables_and_columns.rb
@@ -4,7 +4,7 @@ class RemoveDeprecatedAiTablesAndColumns < ActiveRecord::Migration[8.0]
     drop_table :ai_elements
     drop_table :ai_messages
     drop_table :ai_tasks
-    drop_table :ai_themes
+    drop_table :ai_themes, if_exists: true
     remove_column :papers, :ai_style_id, :integer, default: 0
     remove_column :papers, :ai_theme_id, :integer
   end
