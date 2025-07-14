db88d3efecadfd6750f29e1b1c25058052eee7d7
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-07-14 15:02:19 +0200

    fix migration order

--- db/migrate/20250712032334_remove_deprecated_ai_tables_and_columns.rb
+++ db/migrate/20250712032334_remove_deprecated_ai_tables_and_columns.rb
@@ -1,11 +1,12 @@
 class RemoveDeprecatedAiTablesAndColumns < ActiveRecord::Migration[8.0]
   def change
-    drop_table :ai_elements_themes
-    drop_table :ai_elements
-    drop_table :ai_messages
-    drop_table :ai_tasks
-    drop_table :ai_themes, if_exists: true
-    remove_column :papers, :ai_style_id, :integer, default: 0
     remove_column :papers, :ai_theme_id, :integer
+    remove_column :papers, :ai_style_id, :integer, default: 0
+
+    drop_table :ai_elements_themes, if_exists: true
+    drop_table :ai_elements, if_exists: true
+    drop_table :ai_messages, if_exists: true
+    drop_table :ai_tasks, if_exists: true
+    drop_table :ai_themes, if_exists: true
   end
 end
