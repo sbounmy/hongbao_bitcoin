a67be0ddfc4f8268fcf6127bd4edad3c52247fe5
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-07-16 16:11:22 +0200

    fix spec

--- e2e/playwright/e2e/input_events.spec.js
+++ e2e/playwright/e2e/input_events.spec.js
@@ -8,7 +8,7 @@ test.describe("Input Events", () => {
     await page.goto('/inputs/15');
     await expect(page.locator('body')).toContainText('Pizza Day');
     await expect(page.locator('body')).toContainText('May 22, 2010');
-    await expect(page.locator('body')).toContainText('Pizza Day is the first commercial transaction on the Bitcoin network. 2 Pizzas were bought for 10,000 BTC by Laszlo Hanyecz, a Bitcoin enthusiast and was the first GPU miner.');
+    await expect(page.locator('body')).toContainText("Celebrates the very first \'real world\'");
     await expect(page.locator('.papers-item-component')).toHaveCount(1);
   });
 });
