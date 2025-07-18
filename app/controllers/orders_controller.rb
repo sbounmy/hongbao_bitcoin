1889b2766c911e191c41ef3db045262d583538e8
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-07-18 15:07:03 +0100

    chore: remove status polling

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,4 @@
 class OrdersController < ApplicationController
-  allow_unauthenticated_access only: [ :status ]
-
   def index
     @orders = current_user.orders.includes(:line_items, :tokens).order(created_at: :desc)
   end
@@ -8,13 +6,4 @@ class OrdersController < ApplicationController
   def show
     @order = current_user.orders.find(params[:id])
   end
-
-  def status
-    @order = Order.find(params[:id])
-    
-    if params[:content_only].present?
-      render Orders::StatusComponent.new(order: @order), layout: false
-    end
-  end
-
 end
