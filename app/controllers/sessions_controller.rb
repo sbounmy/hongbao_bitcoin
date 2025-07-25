bb0f6cb875303f741950eb4f130e1a0e0d6d48a3
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-07-25 10:17:31 +0100

    feat: remove header and footer, create authentication layout to remove footer, replace new_session with login_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   allow_unauthenticated_access only: [ :new, :create ]
-  layout "main"
-  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "Try again later." }
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to login_path, alert: "Try again later." }
+  layout "authentication", only: [ :new ]
   def new
   end
 
