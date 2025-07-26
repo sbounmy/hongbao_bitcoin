15dfa1d580baa6b5834bcda4a4513b9172e82131
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-07-26 16:08:40 +0100

    feat: refactor turbo stream code into the view component

--- app/components/infinite_scroll_component.rb
+++ app/components/infinite_scroll_component.rb
@@ -30,4 +30,39 @@ class InfiniteScrollComponent < ApplicationComponent
   def next_page_path
     path_method.call(page: pagy.next, format: :turbo_stream)
   end
+
+  # Generate turbo stream response for appending items and updating loader
+  def turbo_stream_response
+    safe_join([
+      turbo_stream.send(turbo_stream_action, container_id) do
+        render collection
+      end,
+      turbo_stream.replace(loader_frame_id) do
+        render_next_page_loader
+      end
+    ])
+  end
+
+  private
+
+  def loader_frame_id
+    "#{container_id}_page_#{pagy.page}"
+  end
+
+  def render_next_page_loader
+    return "" unless pagy.next
+
+    turbo_frame_tag loader_id,
+                    src: next_page_path,
+                    loading: :lazy do
+      tag.div(class: "col-span-full flex justify-center items-center py-8") do
+        tag.div(class: "flex flex-col items-center gap-2") do
+          safe_join([
+            tag.span(class: "loading loading-spinner loading-lg text-primary"),
+            tag.span(loader_text, class: "text-base-content/60")
+          ])
+        end
+      end
+    end
+  end
 end
