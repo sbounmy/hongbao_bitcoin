3a3af47b99f860ad12a48e87d7976ec95f6312be
sMouaad <93816869+sMouaad@users.noreply.github.com>
2025-07-31 18:31:56 +0100

    fix: add more timeout to stripe manage billing page

--- e2e/playwright/e2e/stripe.spec.js
+++ e2e/playwright/e2e/stripe.spec.js
@@ -68,7 +68,7 @@ test.describe('Stripe Checkout Flow', () => {
     await billingButton.click();
 
     await page.waitForURL('https://billing.stripe.com/p/session/**');
-    await page.waitForLoadState('networkidle');
+    await page.waitForLoadState('networkidle', { timeout: 120000 }); // 120 seconds timeout
     
     await expect(page.locator('body')).toContainText("Invoice history");
     await appVcrEjectCassette();
