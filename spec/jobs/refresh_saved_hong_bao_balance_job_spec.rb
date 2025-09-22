9f002ad68d3ab473c3cbbb3db9bcbf349406c3b9
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-09-22 10:04:47 +0700

    fix job spec

--- spec/jobs/refresh_saved_hong_bao_balance_job_spec.rb
+++ spec/jobs/refresh_saved_hong_bao_balance_job_spec.rb
@@ -1,62 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe RefreshSavedHongBaoBalanceJob, type: :job do
-  let(:user) { User.create!(email: "test@example.com", password: "password123") }
-  let(:saved_hong_bao) do
-    SavedHongBao.create!(
-      user:,
-      name: "Test Hong Bao",
-      address: "bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh"
-    )
-  end
+  fixtures :users, :saved_hong_baos
 
-  let(:balance_mock) { instance_double(Balance) }
-  let(:transaction_mock) { double(satoshis: 100000, timestamp: 2.days.ago) }
-  let(:spot_mock) { instance_double(Spot) }
+  let(:saved_hong_bao) { saved_hong_baos(:created) }
 
-  before do
-    allow(Balance).to receive(:new).with(address: saved_hong_bao.address).and_return(balance_mock)
-    allow(balance_mock).to receive(:transactions).and_return([ transaction_mock ])
-    allow(balance_mock).to receive(:satoshis).and_return(150000)
-
-    allow(Spot).to receive(:new).and_return(spot_mock)
-    allow(spot_mock).to receive(:to).with(:usd).and_return(45000.0)
-    allow(Spot).to receive(:current).with(:usd).and_return(50000.0)
-  end
-
-  describe "#perform" do
+  describe "#perform", vcr: { cassette_name: "refresh_saved_hong_bao_balance_job" } do
     it "updates the saved hong bao with current balance information" do
       expect {
         described_class.new.perform(saved_hong_bao.id)
         saved_hong_bao.reload
-      }.to change { saved_hong_bao.current_sats }.from(nil).to(150000)
+      }.to change { saved_hong_bao.current_sats }.from(nil)
+      expect(saved_hong_bao).to have_attributes(
+        initial_sats: 41171,
+        initial_spot: 0,
+        current_sats: 41171,
+        current_spot: 0.11429127e6,
+        gifted_at: be_within(1.day).of(Time.utc(2025, 6, 22, 03)),
+        last_fetched_at: be_present
+      )
     end
 
-    it "sets initial_sats if not already set" do
-      saved_hong_bao.update!(initial_sats: nil)
+    it "does not sets initial valus if already set" do
+      saved_hong_bao.update!(initial_sats: 1, initial_spot: 2)
 
-      described_class.new.perform(saved_hong_bao.id)
-      saved_hong_bao.reload
-
-      expect(saved_hong_bao.initial_sats).to eq(100000)
-    end
-
-    it "does not override initial_sats if already set" do
-      saved_hong_bao.update!(initial_sats: 50000)
-
-      described_class.new.perform(saved_hong_bao.id)
-      saved_hong_bao.reload
-
-      expect(saved_hong_bao.initial_sats).to eq(50000)
-    end
-
-    it "sets initial_spot if not already set" do
-      saved_hong_bao.update!(initial_spot: nil)
-
-      described_class.new.perform(saved_hong_bao.id)
-      saved_hong_bao.reload
-
-      expect(saved_hong_bao.initial_spot).to eq(45000.0)
+      expect {
+        expect {
+          described_class.new.perform(saved_hong_bao.id)
+          saved_hong_bao.reload
+        }.not_to change { saved_hong_bao.initial_sats }.from(1)
+      }.not_to change { saved_hong_bao.initial_spot }.from(2)
     end
 
     it "sets gifted_at from first transaction if not already set" do
@@ -65,36 +38,24 @@ RSpec.describe RefreshSavedHongBaoBalanceJob, type: :job do
       described_class.new.perform(saved_hong_bao.id)
       saved_hong_bao.reload
 
-      expect(saved_hong_bao.gifted_at).to eq(transaction_mock.timestamp)
+      expect(saved_hong_bao.gifted_at).not_to be_nil
     end
 
     it "updates current_spot with current market price" do
       described_class.new.perform(saved_hong_bao.id)
       saved_hong_bao.reload
 
-      expect(saved_hong_bao.current_spot).to eq(50000.0)
+      expect(saved_hong_bao.current_spot).to be > 0
     end
 
     it "updates last_fetched_at timestamp" do
+      saved_hong_bao.update!(last_fetched_at: nil)
       time_before = Time.current
+
       described_class.new.perform(saved_hong_bao.id)
       saved_hong_bao.reload
 
       expect(saved_hong_bao.last_fetched_at).to be_within(1.second).of(time_before)
-    end
-
-    it "handles addresses with no transactions" do
-      allow(balance_mock).to receive(:transactions).and_return([])
-      allow(balance_mock).to receive(:satoshis).and_return(0)
-
-      described_class.new.perform(saved_hong_bao.id)
-      saved_hong_bao.reload
-
-      # When there are no transactions, initial_sats gets set to 0 from balance.satoshis
-      expect(saved_hong_bao.initial_sats).to eq(0)
-      expect(saved_hong_bao.initial_spot).to eq(45000.0)  # Still gets set from Spot.new
-      expect(saved_hong_bao.gifted_at).to be_nil
-      expect(saved_hong_bao.current_sats).to eq(0)
     end
 
     it "raises error if saved hong bao not found" do
