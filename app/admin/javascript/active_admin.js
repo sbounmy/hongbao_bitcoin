7b02fab235ce8862789465722b8ba92f39af30ec
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-09-30 10:48:28 +0700

    fix: disable turbo for activeadmin to fix sidebar

--- app/admin/javascript/active_admin.js
+++ app/admin/javascript/active_admin.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "./turbo_config"  // Configure Turbo before loading anything else
 import "@activeadmin/activeadmin";
 import "@hotwired/turbo-rails"
 import "./controllers"
