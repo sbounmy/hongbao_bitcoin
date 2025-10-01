3feae7260b43799a7d3c276343aac518eeb017c7
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-10-01 11:56:41 +0700

    feat: description for variant in order summary

---
+++ app/components/variants/description_component.rb
@@ -0,0 +1,26 @@
+module Variants
+  class DescriptionComponent < ApplicationComponent
+    def initialize(variant:)
+      @variant = variant
+    end
+
+    private
+
+    attr_reader :variant
+
+    def color_option_values
+      @color_option_values ||= variant.color_option_values
+    end
+
+    def envelopes_per_color
+      return 0 unless color_option_values.any?
+
+      total_envelopes = variant.product.metadata["envelopes_count"] || 0
+      total_envelopes / color_option_values.size
+    end
+
+    def render?
+      color_option_values.any?
+    end
+  end
+end
