28893c01dcbe3b0f58a4e6b2cf71753535be770e
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-10-15 09:07:39 +0700

    fix ordering HB and portfolio height scrollable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   allow_unauthenticated_access
   def index
     @users = User.joins(:avatar_attachment).with_attached_avatar.limit(8)
-    @saved_hong_baos = User.find_by(email: "stephane@hackerhouse.paris")&.saved_hong_baos || SavedHongBao.none
+    @saved_hong_baos = (User.find_by(email: "stephane@hackerhouse.paris")&.saved_hong_baos || SavedHongBao.none).order(gifted_at: :desc)
   end
 
   def pricing
