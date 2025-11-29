5d65fe235e4c70a4a2ccb8685f1fc46e1b7a7664
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-11-30 06:08:09 +0700

    fix: make it trading card standard size so its easier for storage like protector etc :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   safelist: [
     'aspect-[150/75]',  // Landscape aspect ratio
-    'aspect-[75/113]',  // Portrait aspect ratio
+    'aspect-[63/88]',   // Portrait aspect ratio (standard trading card)
   ],
   // ... rest of your config
 }
