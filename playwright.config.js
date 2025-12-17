2e57f559a17eadcdff5308db3b5e56998764c73c
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2025-12-18 00:09:15 +0700

    increase timeout config for ci

--- playwright.config.js
+++ playwright.config.js
@@ -27,6 +27,7 @@ export default defineConfig({
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: isParallelRun ? 'blob' : 'html',
+  timeout: process.env.CI ? 45_000 : 30_000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
