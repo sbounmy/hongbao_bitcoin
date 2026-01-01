1ba0e1bac2f421becad55bfbc910ca6262e549cb
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2026-01-01 09:11:12 +0700

    fix offense

--- db/migrate/20260101020513_remove_likes_and_bundles.rb
+++ db/migrate/20260101020513_remove_likes_and_bundles.rb
@@ -22,7 +22,7 @@ class RemoveLikesAndBundles < ActiveRecord::Migration[8.0]
       t.string "name"
       t.string "status"
       t.timestamps
-      t.index ["user_id"], name: "index_bundles_on_user_id"
+      t.index [ "user_id" ], name: "index_bundles_on_user_id"
     end
   end
 end
