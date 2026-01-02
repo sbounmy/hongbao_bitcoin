a9d5d4d4c916824bb1b972a323cdcdb05e048027
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2026-01-02 08:10:11 +0700

    chore: spec activable

---
+++ spec/models/concerns/activable_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Activable, type: :concern do
+  before(:all) do
+    ActiveRecord::Schema.define do
+      create_table :posts, force: true do |t|
+        t.boolean :active, default: true, null: false
+      end
+    end
+
+    class Post < ActiveRecord::Base
+      include Activable
+    end
+  end
+
+  let(:post) { Post.create! }
+
+  describe '.active' do
+    it 'returns only active records' do
+      active = Post.create!(active: true)
+      inactive = Post.create!(active: false)
+
+      expect(Post.active).to include(active)
+      expect(Post.active).not_to include(inactive)
+    end
+  end
+
+  describe '.inactive' do
+    it 'returns only inactive records' do
+      active = Post.create!(active: true)
+      inactive = Post.create!(active: false)
+
+      expect(Post.inactive).to include(inactive)
+      expect(Post.inactive).not_to include(active)
+    end
+  end
+end
